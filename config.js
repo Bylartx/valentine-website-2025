8632dd8bccfbf5b0ed227195bbaa61721a39cb4a
Bylartx <villanueva2435@hotmail.com>
2025-02-13 19:18:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "ANNI",
+    valentineName: "Anni",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=rF5tJ8xuaIc&list=PLbqsLXjyXw3iFsEgQuVQOKcYZ2MUsmKey&index=5", // Music streaming URL
+        musicUrl: "https://soundcloud.com/user-126922082/jorge-rivera-herrans-would-you-fall-in-love-with-me-again", // Music streaming URL
         startText: "🎵 Would you fall in love with me again?",        // Button text to start music
         stopText: "🔇 Dejar de escuchar esta obra maestra",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
